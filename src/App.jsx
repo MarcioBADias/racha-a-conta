36e1d1345230f73286c2ee32bdf48a60e3b3a6f9
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-11-18 19:20:48 -0300

    ✅ Criado header com logo e titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 const App = () => {
   return (
     <>
-      <h1>Racha a conta</h1>
+      <header>
+        <img src="logo-racha-conta.png" alt="Logo e Titulo: Racha a conta" />
+      </header>
+      <div className="sidebar">
+        <ul>
+          <li>
+
+          </li>
+        </ul>
+        <button className="button">Adicionar Amigo(a)</button>
+      </div>
     </>
   )
 }
