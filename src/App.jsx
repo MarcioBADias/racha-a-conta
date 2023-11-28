1159b128808263b12ac94d35bdb99a2fc64533d6
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-11-28 08:21:52 -0300

    ⚠ Correção da getMsgInfo e na estrutura do app()

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,57 @@
+const friends = [
+  {
+    id: crypto.randomUUID(),
+    name: 'Henrique',
+    amount: -7,
+    img: `/imgs/friends/henrique-48.jpg`
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Renata',
+    amount: 20,
+    img: `/imgs/friends/renata-48.jpg`
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Antonio',
+    amount: 0,
+    img: `/imgs/friends/antonio-48.jpg`
+  }
+]
+
+const getMsgInfo = amount => amount < 0 ?
+  { message: `você deve ${Math.abs(amount)} reais`, color: 'red-debit' } :
+  amount > 0 ?
+    { message: `te devo ${amount} reais`, color: 'green-credit' } :
+    { message: 'Estamos quites', color: 'white-neutral' }
+
 const App = () => {
+
   return (
     <>
       <header>
         <img src="logo-racha-conta.png" alt="Logo e Titulo: Racha a conta" />
       </header>
-      <div className="sidebar">
-        <ul>
-          <li>
-
-          </li>
-        </ul>
-        <button className="button">Adicionar Amigo(a)</button>
-      </div>
+      <main className="app">
+        <div className="sidebar">
+          <ul>
+            {
+              friends.map(friend => {
+                const { color, message } = getMsgInfo(friend.amount)
+                return (
+                  <li key={friend.id}>
+                    <img src={friend.img} alt={`Foto de ${friend.name}`} />
+                    <h3>{friend.name}</h3>
+                    <p className={color}>{message}</p>
+                    <button className="button">Selecionar</button>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          <button className="button">Adicionar Amigo(a)</button>
+        </div >
+      </main>
     </>
   )
 }
