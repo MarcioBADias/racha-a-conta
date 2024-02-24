c05e734b9f867adb14318d1116171eb144abe4ca
marciobadias <marcio.b.a.dias@gmail.com>
2024-02-23 23:10:32 -0300

    ✅ Passando o state a reducer

--- src/hooks/useFriends.jsx
+++ src/hooks/useFriends.jsx
@@ -1,48 +1,75 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 import localforage from 'localforage'
+
+const reduce = (state, action) =>
+  ({
+    get_friends: {
+      ...state,
+      friends: action.payload?.length > 0 ? action.payload : [],
+    },
+    set_form_add_friends: {
+      ...state,
+      showFormAddFriend: !state.showFormAddFriend,
+    },
+    set_select_friend: {
+      ...state,
+      selectFriend:
+        state.selectFriend?.id === action.payload?.id ? null : action.payload,
+    },
+    set_friends: {
+      ...state,
+      friends: state.friends.map((p) =>
+        action.payload?.id === p.id ? action.payload : p,
+      ),
+    },
+    add_friend: {
+      ...state,
+      friends: [...state.friends, action.payload],
+    },
+  })[action.type] ?? state
+
 const useFriends = () => {
-  const [friends, setFriends] = useState([])
-  const [selectFriend, setSelectFriend] = useState(null)
-  const [showFormAddFriend, setShowFormAddFriend] = useState(false)
+  const [state, dispatch] = useReducer(reduce, {
+    friends: [],
+    selectFriend: null,
+    showFormAddFriend: false,
+  })
 
   useEffect(() => {
     localforage
-      .setItem('friends', friends)
+      .setItem('friends', state.friends)
       .catch((error) => alert(error.message))
-  }, [friends])
+  }, [state.friends])
 
   useEffect(() => {
     localforage
       .getItem('friends')
       .then((data) => {
         if (data) {
-          setFriends(data)
+          dispatch({ type: 'get_friends', payload: data })
         }
       })
       .catch((error) => alert(error.message))
   }, [])
 
-  const handleClickAddFriend = () => setShowFormAddFriend((b) => !b)
+  const handleClickAddFriend = () => dispatch({ type: 'set_form_add_friends' })
   const handleClickFriend = (friend) =>
-    setSelectFriend((p) => (p?.id === friend.id ? null : friend))
+    dispatch({ type: 'set_select_friend', payload: friend })
 
   const handleSubmitShareBill = (friend) => {
-    setFriends((prev) => prev.map((p) => (friend.id === p.id ? friend : p)))
-    setSelectFriend(null)
+    dispatch({ type: 'set_friends', payload: friend })
+    dispatch({ type: 'set_select_friend', payload: friend })
   }
 
   const handleSubmitAddFriend = (friend) => {
-    setFriends((prev) => [...prev, friend])
-    setShowFormAddFriend(false)
+    dispatch({ type: 'add_friend', payload: friend })
+    dispatch({ type: 'set_form_add_friends' })
   }
 
   return {
-    friends,
-    selectFriend,
-    showFormAddFriend,
-    setFriends,
-    setSelectFriend,
-    setShowFormAddFriend,
+    friends: state.friends,
+    selectFriend: state.selectFriend,
+    showFormAddFriend: state.showFormAddFriend,
     handleClickAddFriend,
     handleClickFriend,
     handleSubmitShareBill,
